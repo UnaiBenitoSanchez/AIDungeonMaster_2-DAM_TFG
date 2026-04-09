879402525f825b51c552e559d859437f4f9c2742
UnaiBenitoSanchez <unaibenitos@gmail.com>
2026-04-09 10:53:11 +0200

    Pequeños ajustes para que el arma usada en combate sea el arma equipada, uso real de los objetos al cambiar estadísticas y estadísticas mostradas en el apartado del ivnentario.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "2.4.1"
+        versionName = "2.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
