c2d54341515ff0f9065348ef8161bedafb6ad9c1
UnaiBenitoSanchez <unaibenitos@gmail.com>
2026-04-26 15:35:23 +0200

    Actualización de versión.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "3.4.2"
+        versionName = "3.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
